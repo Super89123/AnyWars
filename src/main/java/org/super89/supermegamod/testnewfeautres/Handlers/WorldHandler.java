854ba193837d2e9e60d2b30fcb6ebdf9e313f240
Super89 <tedswa@mail.ru>
2024-08-12 19:56:07 +0400

    Created Generators and Api package and VoidGenerator.java . Fixed WorldHandler.java

--- src/main/java/org/super89/supermegamod/testnewfeautres/Handlers/WorldHandler.java
+++ src/main/java/org/super89/supermegamod/testnewfeautres/Handlers/WorldHandler.java
@@ -1,19 +1,24 @@
 package org.super89.supermegamod.testnewfeautres.Handlers;
 
+import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.WorldCreator;
+import org.super89.supermegamod.testnewfeautres.Generators.VoidGenerator;
 
 public class WorldHandler {
     private final String world_name;
     private final World generatedWorld;
-    public WorldHandler(String world_name, World generatedWorld){
+    public WorldHandler(String world_name){
         this.world_name = world_name;
 
-        this.generatedWorld = generatedWorld;
+
         WorldCreator worldCreator = new WorldCreator(world_name);
         worldCreator.environment(World.Environment.NORMAL);
         worldCreator.generateStructures(false);
-        worldCreator.generator()
+        worldCreator.generator(new VoidGenerator());
+
+
+        this.generatedWorld = Bukkit.createWorld(worldCreator);
 
         // TODO
     }
